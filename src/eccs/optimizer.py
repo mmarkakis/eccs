9caf40f2ecf9f35b279288349fabd2fd67fbcaba
Markos Markakis <markakis@mit.edu>
2024-02-14 13:11:35 -0500

    Get gurobi-based optimizer to a runnable state

--- src/eccs/optimizer.py
+++ src/eccs/optimizer.py
@@ -28,11 +28,11 @@ class Optimizer:
     def optimize(
         self,
         ATE_init: float,
-        excl_set: list[int],
-        incl_set: list[int],
+        excl_set: set[int],
+        incl_set: set[int],
         lambda_1: float,
         lambda_2: float,
-    ) -> Optional[list[int]]:
+    ) -> Optional[set[int]]:
         """Find the ATE-change-maximizing adjustment set.
 
         Parameters:
@@ -48,7 +48,7 @@ class Optimizer:
 
         y = self._data.iloc[:, self._outcome_idx].values
         X = (
-            self._data
+            self._data.to_numpy()
         )  # Keep y here to avoid changing the variable indexing. We ensure that we don't actually regress y
         # on itself by adding it to the excl_set later on.
 
@@ -58,21 +58,16 @@ class Optimizer:
         # Decision variables
         N = self._data.shape[0]
         M = self._data.shape[1]
-        beta = model.addVars(M, lb=-GRB.INFINITY, ub=GRB.INFINITY, name="beta")
+        beta = model.addVars(
+            M, vtype=GRB.CONTINUOUS, lb=-GRB.INFINITY, ub=GRB.INFINITY, name="beta"
+        )
         A = model.addVars(M, vtype=GRB.BINARY, name="A")
+        MSE_sup = model.addVar(vtype=GRB.CONTINUOUS, name="MSE_sup")
 
         # Update model to integrate new variables
         model.update()
 
         # Objective function components
-        # Define the MSE part of the objective
-        MSE = (
-            quicksum(
-                (y[i] - quicksum(beta[j] * A[j] * X[i, j] for j in range(M))) ** 2
-                for i in range(N)
-            )
-            / N
-        )
 
         # Penalty for the distance of beta_1 from ATE_init
         ATE_diff = (ATE_init - beta[self._treatment_idx]) * (
@@ -81,21 +76,32 @@ class Optimizer:
 
         # Sparsity penalty
         non_free_vars = (
-            excl_set.union(incl_set).union(self._outcome_idx).union(self._treatment_idx)
+            excl_set.union(incl_set).union(set([self._outcome_idx])).union(set([self._treatment_idx]))
         )
         sparsity_penalty = quicksum(A[j] for j in range(M) if j not in non_free_vars)
 
         # Set objective
         model.setObjective(
-            MSE + lambda_1 * sparsity_penalty - lambda_2 * ATE_diff, GRB.MINIMIZE
+            MSE_sup + lambda_1 * sparsity_penalty - lambda_2 * ATE_diff, GRB.MINIMIZE
         )
 
         # Constraints for A_j based on excl_set and incl_set
-        for j in excl_set.union(self._outcome_idx):
+        for j in excl_set.union(set([self._outcome_idx])):
             model.addConstr(A[j] == 0, "Exclude_%d" % j)
 
-        for j in incl_set.union(self._treatment_idx):
+        for j in incl_set.union(set([self._treatment_idx])):
             model.addConstr(A[j] == 1, "Include_%d" % j)
+
+        # Constraint for MSE_sup
+        model.addConstr(
+            MSE_sup
+            >= quicksum(
+                (y[i] - quicksum(X[i, j] * beta[j] for j in range(M))) ** 2
+                for i in range(N)
+            )
+            / N,
+            "MSE_sup",
+        )
 
         # Solve model
         model.optimize()
