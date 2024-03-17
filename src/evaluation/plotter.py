6efaaca40763fdc6a5c8b0330b1fab0f3c31d5e7
Markos Markakis <markakis@mit.edu>
2024-03-17 14:31:28 -0400

    Decide number of data points based on data

--- src/evaluation/plotter.py
+++ src/evaluation/plotter.py
@@ -29,20 +29,19 @@ def main():
     data_best = np.load(file_path_best, allow_pickle=True)
     data_random = np.load(file_path_random, allow_pickle=True)
 
-    num_data_points = 10
     plt.plot(
-        range(num_data_points), data_best[:num_data_points], label="Best", marker="o"
+        range(len(data_best)), data_best, label="Best", marker="o"
     )
     plt.plot(
-        range(num_data_points),
-        data_random[:num_data_points],
+        range(len(data_best)),
+        data_random[:len(data_best)],
         label="Random",
         marker="o",
     )
     plt.xlabel("User Interaction Index")
     plt.ylabel("Graph Edit Distance from Ground Truth")
     plt.yticks(range(0, int(max(max(data_best), max(data_random))) + 1))
-    plt.xlim(0, num_data_points)
+    plt.xlim(0, len(data_best))
     plt.legend()
     plt.tight_layout()
     plt.savefig(
@@ -64,20 +63,19 @@ def main():
     data_best = np.load(file_path_best, allow_pickle=True)
     data_random = np.load(file_path_random, allow_pickle=True)
 
-    num_data_points = 10
     plt.cla()
     plt.plot(
-        range(num_data_points), data_best[:num_data_points], label="Best", marker="o"
+        range(len(data_best)), data_best, label="Best", marker="o"
     )
     plt.plot(
-        range(num_data_points),
-        data_random[:num_data_points],
+        range(len(data_best)),
+        data_random[:len(data_best)],
         label="Random",
         marker="o",
     )
     plt.xlabel("User Interaction Index")
     plt.ylabel("Absolute ATE Difference")
-    plt.xlim(0, num_data_points)
+    plt.xlim(0, len(data_best))
     plt.legend()
     plt.tight_layout()
     plt.savefig(
