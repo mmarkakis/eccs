9592d4ef91991d7777ee1115de4cc33b520b1f80
SylviaZiyuZhang <sylziyuz@csail.mit.edu>
2024-03-18 18:20:58 -0400

    fixed some things in the A star code

--- src/eccs/heuristic_search.py
+++ src/eccs/heuristic_search.py
@@ -2,7 +2,7 @@ import dihash
 import heapq
 import math
 import networkx as nx
-from typing import List, Optional
+from typing import List, Optional, Tuple
 
 from .ate import ATECalculator
 
@@ -14,6 +14,8 @@ class AStarSearch:
         self.m = len(init_graph.edges())
         self.n = len(init_graph.nodes())
         self.init_graph = init_graph
+        for n in self.init_graph.nodes: # This is some hacky code to make it work with graph hashing
+            self.init_graph.nodes[n]['label'] = self.init_graph.nodes[n]['var_name']
         self.treatment = treatment
         self.outcome = outcome
         self.data = data
@@ -42,16 +44,25 @@ class AStarSearch:
         try:
             return self._ATE_cache[id]
         except KeyError:
-            ate = ATECalculator.get_ate_and_confidence(
-                data=self.data,
-                treatment=str(self.treatment),
-                outcome=str(self.outcome),
-                graph=graph,
-                calculate_p_value=True,
-                calculate_std_error=True,
-                get_estimand=False,
-            )
-            self._ATE_cache[id] = ate
+            try:
+                ate = ATECalculator.get_ate_and_confidence(
+                    data=self.data,
+                    treatment=str(self.treatment),
+                    outcome=str(self.outcome),
+                    graph=graph,
+                    calculate_p_value=True,
+                    calculate_std_error=True,
+                    get_estimand=False,
+                )
+                self._ATE_cache[id] = ate
+            except ValueError: # Returned by handler of nx.exception.NodeNotFound
+                # Special case of treatment and outcome being not connected
+                ate = {
+                    "ATE": 0,
+                    "P-value": 1,
+                    "Standard Error": 0
+                }
+                self._ATE_cache[id] = ate
             return ate
     
     def _get_potential(self, id: int, graph: nx.DiGraph):
@@ -65,7 +76,7 @@ class AStarSearch:
         # TODO: what is the type of Estimand
         # TODO: implement caching for ATE information
         ATE_info = self._get_ATE_info(id, graph)
-        return ATE_info["ATE"] - self.gamma_1 * ATE_info["Standard Error"] + ATE_info["P-value"] + math.abs(len(v.edges() - self.n))
+        return ATE_info["ATE"] - self.gamma_1 * ATE_info["Standard Error"] + ATE_info["P-value"] + abs(len(graph.edges()) - self.m)
 
     def _explore_neighbor(self, graph: nx.DiGraph, n1: int, n2: int, is_add: bool) -> Optional[int]:
         # the type of nx node is int unless DiGraph.nodes() was called with data options
@@ -102,14 +113,16 @@ class AStarSearch:
             graph.remove_edge(n1, n2)
         else:
             graph.add_edge(n1, n2)
-        return result
+        return (result, (n1, n2, is_add))
     
-    def _get_neighbors(self, current_node_id: int) -> List[int]:
+    def _get_neighbors(self, current_node_id: int) -> List[Tuple[int, Tuple[int, int, bool]]]:
         # Returns list of neighbors
         graph: nx.DiGraph = self._id_to_graph[current_node_id]
         results = []
         for n1 in list(graph.nodes):
             for n2 in list(graph.nodes):
+                if n2 == n1:
+                    continue # skip self-loops
                 neighbor_res = None
                 if graph.has_edge(n1, n2):
                     neighbor_res = self._explore_neighbor(graph, n1, n2, is_add=False)
@@ -117,7 +130,7 @@ class AStarSearch:
                     continue # Adding this edge his creates a cycle
                 else:
                     neighbor_res = self._explore_neighbor(graph, n1, n2, is_add=True)
-                if neighbor_res is not None:
+                if neighbor_res[0] is not None:
                     results.append(neighbor_res)
         
         return results
@@ -144,29 +157,37 @@ class AStarSearch:
             hash_nodes=False,
             apply_quotient=False
         )
-        self._visited[0] = self.init_graph # store actual graph here
+        self._visited.add(0) # store actual graph here
         self._hashtag_to_id[start_hash] = 0
+        self._id_to_graph[0] = self.init_graph
         g_score = {}
         # starting node always has id 0
-        g_score[0] = 0
         f_score = {}
         f_score[0] =  - self._get_potential(0, self.init_graph)
+        # g_score[0] = self._init_potential + f_score[0]
+        g_score[0] = f_score[0]
 
         top_k_candidates = []
 
         while frontier:
+            #print("Starting a loop")
             _, current_node_id = heapq.heappop(frontier)
             self._visited.add(current_node_id)
-            heapq.heappush(top_k_candidates, (self._get_potential(current_node_id, self._id_to_graph[current_node_id], current_node_id)))
+            heapq.heappush(top_k_candidates, (self._get_potential(current_node_id, self._id_to_graph[current_node_id]), current_node_id))
             if len(top_k_candidates) > k:
                 heapq.heappop(top_k_candidates)
-
-            for neighbor_id, edge_type in self._get_neighbors(current_node_id):
+            neighbors = self._get_neighbors(current_node_id)
+            #print(neighbors)
+            for neighbor_id, edge_type in neighbors:
                 # when expanding neighbors, just discard ones that are too low p-value
-                tentative_g_score = g_score[current_node_id] + self._init_potential - self._get_potential(current_node_id, self._id_to_graph[current_node_id])
-                neighbor_g_score = g_score.get(neighbor_id, self._init_potential - self._get_potential(current_node_id, self._id_to_graph[current_node_id]))
+                # tentative_g_score = g_score[current_node_id] + self._init_potential - self._get_potential(current_node_id, self._id_to_graph[current_node_id])
+                tentative_g_score = g_score[current_node_id]
+                neighbor_g_score = g_score.get(neighbor_id, self._init_potential - self._get_potential(neighbor_id, self._id_to_graph[neighbor_id]))
+                #print(tentative_g_score)
+                #print(neighbor_g_score)
+                # print(self._ATE_cache[neighbor_id]["ATE"])
                 # g_score[neighbor_id] = neighbor_g_score # g(n) in f(n) = g(n) + h(n)
-                if tentative_g_score < neighbor_g_score:
+                if tentative_g_score <= neighbor_g_score:
                     self._predecessors[neighbor_id] = (current_node_id, edge_type)
                     g_score[neighbor_id] = tentative_g_score
                     # f_score[neighbor_id] = tentative_g_score + self.heuristic(neighbor)
@@ -180,16 +201,18 @@ class AStarSearch:
         for p in top_k_candidates:
             c_id = p[0]
             while c_id in self._predecessors:
-                n1, n2, _ = self._predecessors[c_id]
+                c_id, (n1, n2, _) = self._predecessors[c_id]
                 if (n1, n2) not in edge_tally:
-                    edge_tally[(n1, n2)] = (1, math.abs(self._ATE_cache[c_id]["ATE"] - self.ATE_init))
+                    edge_tally[(n1, n2)] = (1, abs(self._ATE_cache[c_id]["ATE"] - self.ATE_init))
                 else:
                     cnt = edge_tally[(n1, n2)][0] + 1
-                    total_diff = edge_tally[(n1, n2)][1] + math.abs(self._ATE_cache[c_id]["ATE"] - self.ATE_init)
+                    total_diff = edge_tally[(n1, n2)][1] + abs(self._ATE_cache[c_id]["ATE"] - self.ATE_init)
                     edge_tally[(n1, n2)] = (cnt, total_diff)
-                current_node = self._predecessors[current_node]
-
-        sorted_edges = sorted(edge_tally.items(), key=lambda x:x[1][0])
+                # current_node = self._predecessors[current_node]
+        print(top_k_candidates)
+        print("printing edge tally")
+        print(edge_tally)
+        sorted_edges = sorted(edge_tally.items(), key=lambda x:x[1][0], reverse=True)
         print(sorted_edges[:10])
 
         return sorted_edges
